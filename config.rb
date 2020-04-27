3d6925d81b4d9d4e1e99130451f018bd557b21ef
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2020-04-27 23:26:42 +0200

    iniziato a collegare contentful

--- config.rb
+++ config.rb
@@ -5,6 +5,13 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+# activate :contentful do |f|
+#   f.space         = fd82fq972rzy
+#   f.access_token  = ik785ggYFfwzYzq818EBXKkZjXm5YZHMjHoKTliUSPQ
+#   # f.cda_query     = QUERY
+#   f.content_types = homePage
+# end
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
